1327ab8f9496c0214814a46d3edb802d051c3409
Lucas de Lima <lucas2001brito@hotmail.com>
2020-02-24 21:03:37 -0300

    Incremento de Regras para cadastro

--- empresa/cadastro.php
+++ empresa/cadastro.php
@@ -10,18 +10,6 @@ $email = $_POST['txtemail'];
 $senha = md5($_POST['txtsenha']);
 $cep = $_POST['txtcep'];
 $celular = $_POST['txtcelular'];
-$contrg= strlen($rg); // Faz a contagem dos caracteres da varíavel rg.
-
-    // separando yyyy, mm, ddd
-    list($ano, $mes, $dia) = explode('-', $datanascimento);
-
-    // data atual
-    $hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
-    // Descobre a unix timestamp da data de nascimento do fulano
-    $nascimento = mktime( 0, 0, 0, $mes, $dia, $ano);
-
-    // cálculo
-    $idade = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
 
 $sql="INSERT INTO tbempresa (nomecompleto,rg,cpf,dt_nascimento,email,senha,cep,celular) VALUES ('$nomecompleto','$rg','$cpf','$datanascimento','$email','$senha','$cep','$celular')";
 
@@ -35,7 +23,7 @@ else
     exit;
 }
 
-if($contrg == 9 ) //Pega o $contrg e vê quantos caracteres tem no campo rg e só aceita 9 caracteres.
+if(strlen($rg) == 9 ) //Vê quantos caracteres tem na variavel $rg e só aceita 9 caracteres.
 {
     echo "RG Válido";
 }
@@ -45,8 +33,20 @@ else
     exit;
 }
 
-if($idade >= 18) //Ecige a idade minima de 18 anos para se cadastrar no site.
+// separando yyyy, mm, ddd
+    list($ano, $mes, $dia) = explode('-', $datanascimento);
+
+    // data atual
+    $hoje = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+    // Descobre a unix timestamp da data de nascimento do fulano
+    $nascimento = mktime( 0, 0, 0, $mes, $dia, $ano);
+
+    // cálculo
+    $idade = floor((((($hoje - $nascimento) / 60) / 60) / 24) / 365.25);
+
+if($idade >= 18) //Exige a idade minima de 18 anos para se cadastrar no site.
 {
+    
     echo "Idade Válida";
 }
 else
@@ -54,3 +54,65 @@ else
     echo "Idade Inválida";
     exit;
 }
+
+function validacpf($cpf)
+    {
+        $cpf=  preg_replace( '/[^0-9]/is', '', $cpf ); //Extrai os números.
+        
+        
+    if(strlen($cpf) != 11) //Verifica se a quantidade de caracteres é diferente de 11.
+    {
+        return false;
+    }
+    
+    if (preg_match('/(\d)\1{10}/', $cpf)) //Impossibilita a utilização de caracteres alfanumericos repetidos. Exemplo:"111.111.1111-11". 
+    {
+        return false;
+    }
+    
+    //Faz a conta para verificar a autenticidade do cpf. 
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf{$c} * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf{$c} != $d) {
+            return false;
+        }
+    }
+    return true;
+
+    }
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {   
+        
+        echo "E-mail inválido.";    
+    }
+    else
+    {
+        
+        echo "Seu e-mail é ".$email;
+    }
+
+    if(!preg_match('/^[0-9]{5,5}([- ]?[0-9]{3,3})?$/', $cep)) 
+    {
+        echo "CEP inválido.";
+    }
+    else
+    {
+        mysqli_query($conexao, $sql);
+        
+        mysqli_close($conexao);
+        
+        echo "CEP válido";
+        
+    }
+
+
+
+
+
+
+
+
