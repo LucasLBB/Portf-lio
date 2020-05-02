e836d16c2ef2b3ec35d5787cd54745ebe78767e7
Lucas de Lima <lucas2001brito@hotmail.com>
2020-05-02 20:21:44 -0300

    Alterações no layout da área privada do usuário

---
+++ empresa/privado.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include "validalogin.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Privado</title>
+    <link href="headerpriv.css" rel="stylesheet">
+    <meta charset="utf-8">
+</head>
+
+<header>
+    <a href="logout.php" class="sair">Sair</a>
+</header>
+
+<body>
+    <iframe src="headerpriv.html"></iframe>
+
+</body>
+
+</html>
