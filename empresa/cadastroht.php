f50f577bac96890becfacb547a9259ea75fa0e0d
Lucas de Lima <lucas2001brito@hotmail.com>
2020-05-08 17:38:26 -0300

    Atualização da tela de cadastro

--- empresa/cadastroht.php
+++ empresa/cadastroht.php
@@ -5,44 +5,53 @@ include "conexao.php";
 
 <html lang="pt-br">
 
-    <head>
-    <title>Cadastro</title>    
+<head>
+    <title>Cadastro</title>
+    <link href="headerlog.css" rel="stylesheet">
     <link href="cadastro.css" rel="stylesheet">
     <meta charset="utf-8">
-    </head>
-    
-    <body>
-    
-        <form method="POST" action="cadastro.php">
-        
-        <label for="txtnomecompleto">Nome Completo</label>
-        <input type="text" size="25" name="txtnomecompleto" placeholder="Exemplo:Fulano Silva" required><br>
+</head>
 
-        <label for="txtemail">Email</label><br>
-        <input type="text" size="25" name="txtemail" placeholder="exemplo@exemplo.com" required><br>
-            
-        <label for="txtdatanascimento">Data de Nascimento</label><br>
-        <input type="date" size="25" name="txtdatanascimento" required><br>
-            
-        <label for="txtsenha">Senha</label><br>
-        <input type="password" size="25" name="txtsenha" pattern=".{6,20}" minlenght=6 maxlength="20" size="20" placeholder="mín:6 e max:20" required><br>
+<body>
 
-        <label>Estado</label><br>
-        <select name="procurar"><br>
-        <option>Selecione</option><br>
-        <?php
-            $resultEstado = "SELECT * FROM estado";
-            $resultado = mysqli_query($conexao,$resultEstado);
-            while($row = mysqli_fetch_assoc($resultado)){ ?> 
-                <option value="<?php echo $row['id'];?>"><?php echo $row['nome'];?></option><?php
-            } 
+    <iframe src="headerlog.html"></iframe>
+
+    <h1>Cadastro</h1>
+
+    <?php
+        if(isset($_SESSION["error"])){
+            print "<script>alert(\"{$_SESSION['error']}\")</script>";
+        }
+
+        if(isset($_SESSION["Error_consult"])){
+            print "<script>alert(\"{$_SESSION['Error_consult']}\")</script>";
+        }
     ?>
-            
-        <p>
-        <input type="submit" value="Cadastrar Usuário">
-        </p>
-            
-        </form>
-    </body>
-</html>
 
+    <form method="POST" action="cadastro.php">
+
+        <input type="text" size="25" name="txtnomecompleto" placeholder="Nome Completo" class="nome" required><br>
+   
+        <input type="text" size="25" name="txtemail" placeholder="E-mail" class="email" required><br>
+
+        <input type="password" size="25" name="txtsenha" class="senha" pattern=".{6,20}" minlenght=6 maxlength="20" size="20" placeholder="Senha de mín:6 e max:20" required><br>
+
+        <select name="procurar" class="estado" required><br>
+            <?php
+            $resultEstado = "SELECT * FROM estado";
+            $resultado = mysqli_query($conexao, $resultEstado);
+            while ($row = mysqli_fetch_assoc($resultado)) { ?>
+                <option value="<?php echo $row['id']; ?>"><?php echo $row['nome']; ?></option><?php
+            }
+            ?>
+
+        <input type="date" size="80" name="txtdatanascimento" class="data" placeholder="Data de Nascimento" required><br>
+
+            <p>
+                <input type="submit" value="Cadastrar Usuário" class="cadastrar">
+            </p>
+
+    </form>
+</body>
+
+</html>
