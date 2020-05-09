f07df322d1d5c62af3e324f98489730da53f0711
Lucas de Lima <lucas2001brito@hotmail.com>
2020-05-09 16:05:48 -0300

    Foi adicionada mensagens caso dê erro ao cadastrar,assim como

--- empresa/cadastroht.php
+++ empresa/cadastroht.php
@@ -18,16 +18,6 @@ include "conexao.php";
 
     <h1>Cadastro</h1>
 
-    <?php
-        if(isset($_SESSION["error"])){
-            print "<script>alert(\"{$_SESSION['error']}\")</script>";
-        }
-
-        if(isset($_SESSION["Error_consult"])){
-            print "<script>alert(\"{$_SESSION['Error_consult']}\")</script>";
-        }
-    ?>
-
     <form method="POST" action="cadastro.php">
 
         <input type="text" size="25" name="txtnomecompleto" placeholder="Nome Completo" class="nome" required><br>
@@ -37,12 +27,15 @@ include "conexao.php";
         <input type="password" size="25" name="txtsenha" class="senha" pattern=".{6,20}" minlenght=6 maxlength="20" size="20" placeholder="Senha de mín:6 e max:20" required><br>
 
         <select name="procurar" class="estado" required><br>
+
             <?php
+
             $resultEstado = "SELECT * FROM estado";
             $resultado = mysqli_query($conexao, $resultEstado);
             while ($row = mysqli_fetch_assoc($resultado)) { ?>
                 <option value="<?php echo $row['id']; ?>"><?php echo $row['nome']; ?></option><?php
             }
+
             ?>
 
         <input type="date" size="80" name="txtdatanascimento" class="data" placeholder="Data de Nascimento" required><br>
