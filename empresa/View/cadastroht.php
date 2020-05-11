1efd83453b268f823711adb492e8569975059882
Lucas de Lima <lucas2001brito@hotmail.com>
2020-05-11 18:19:38 -0300

    Atualização de Pastas

---
+++ empresa/View/cadastroht.php
@@ -0,0 +1,50 @@
+<?php
+include "../Model/conexao.php";
+?>
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <title>Cadastro</title>
+    <link href="headerlog.css" rel="stylesheet">
+    <link href="cadastro.css" rel="stylesheet">
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+    <iframe src="headerlog.html"></iframe>
+
+    <h1>Cadastro</h1>
+
+    <form method="POST" action="../Controller/cadastro.php">
+
+        <input type="text" size="25" name="txtnomecompleto" placeholder="Nome Completo" class="nome" required><br>
+   
+        <input type="text" size="25" name="txtemail" placeholder="E-mail" class="email" required><br>
+
+        <input type="password" size="25" name="txtsenha" class="senha" pattern=".{6,20}" minlenght=6 maxlength="20" size="20" placeholder="Senha de mín:6 e max:20" required><br>
+
+        <select name="procurar" class="estado" required><br>
+
+            <?php
+
+            $resultEstado = "SELECT * FROM estado";
+            $resultado = mysqli_query($conexao, $resultEstado);
+            while ($row = mysqli_fetch_assoc($resultado)) { ?>
+                <option value="<?php echo $row['id']; ?>"><?php echo $row['nome']; ?></option><?php
+            }
+
+            ?>
+
+        <input type="date" size="80" name="txtdatanascimento" class="data" placeholder="Data de Nascimento" required><br>
+
+            <p>
+                <input type="submit" value="Cadastrar Usuário" class="cadastrar">
+            </p>
+
+    </form>
+</body>
+
+</html>
---
+++ empresa/View/privado.php
@@ -0,0 +1,118 @@
+<?php
+session_start();
+include "../Controller/validalogin.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Privado</title>
+    <link href="headerpriv.css" rel="stylesheet">
+    <link href="telaprincipal.css" rel="stylesheet">
+    <meta charset="utf-8">
+</head>
+
+
+<body>
+    <header>
+        <a href="../Controller/logout.php" class="sair">Sair</a>
+    </header>
+
+    <iframe src="headerpriv.html"></iframe>
+
+    <section>
+        <h1 class="prin">Conheça nosso Software!!!</h1><br>
+
+        <h2 class="sub">
+            Eagle Security
+            Software.
+        </h2>
+
+        <div>
+            <p>
+                <li>Proteção anti-vírus</li><br>
+                <li>Suporte 24 horas</li><br>
+                <li>Manutenção automática</li><br>
+                <li>Atualizador de Software</li><br>
+                <li>Browser Cleaner e Disk Cleaner</li><br>
+                <li>Proteção contra cibercriminosos</li><br>
+                <li>Proteção para pagamentos</li><br>
+                <li>Proteção contra ransomware</li><br>
+            </p>
+
+            <p><a href="" class="download"><strong>Download</strong></a></p>
+
+        </div>
+    </section>
+
+    <section>
+
+        <h2 id="vagas">Vagas</h2>
+
+        <p id="vag"><br>
+            A Web Security necessita dos seguintes profissionais...
+        </p>
+
+        <li class="anl">Analista de Sistemas Júnior</li><br>
+        <p class="anldesc">
+            Diretoria Transformação e Projetos B2C<br>
+            Local de Trabalho: São Paulo<br>
+            Trabalharará com soluções inovadoras com diferenciais eficientes e competitivos<br>
+            Plano de Carreira<br>
+            Salário a combinar<br>
+        </p>
+
+        <h4 class="qual">Qualificações</h4>
+
+        <p class="anli">
+            Conhecimento técnico em Frameworks<br>
+            Application Servers<br>
+            Integração de sistemas<br>
+            Arquitetura<br>
+            Ferramentas de monitoração<br>
+            Design Patterns<br>
+            Log Analytics<br>
+            Debugging<br>
+            Troubleshooting<br>
+
+            <h4 class="labelins">Inscreva-se:</h4><a href="mailto:contatodaempresa@empresa.com?subject=Analista de Sistemas Júnior - Vaga" target="_blank" class="inscreva">contatodaempresa@empresa.com</a>
+        
+        </p>
+        <li class="esf">Engenheiro de Software Pleno</li><br>
+        <p class="esfdesc">
+            Trabalhará em time de qualidade com suporte ao ambiente de produção</br>
+            Desenvolvimento e evolução de plataformas PDCA</br>
+            Atuação em projetos</br>
+            Foco em garantir confiabilidade dos produtos<br>
+            Local de Trabalho: São Paulo<br>
+            Salário a combinar<br>
+        </p>
+        <h4 class="qual">Qualificações</h4>
+        <p class="esfdesc2">
+            Conhecimento técnico em Frameworks<br>
+            Application Servers<br>
+            Integração de sistemas<br>
+            Arquitetura<br>
+            Ferramentas de monitoração<br>
+            Design Patterns<br>
+            Log Analytics<br>
+            Debugging<br>
+            Troubleshooting<br>
+
+            <h4 class="labeleng">Inscreva-se:</h4><a href="mailto:contatodaempresa@empresa.com?subject=Engenheiro de Software Pleno - Vaga" target="_blank" class="inscreva">contatodaempresa@empresa.com</a><br>
+
+        </p>
+    </section>
+
+    <footer>
+        <hr class="hedfoot">
+
+        <h2 id="contato">Contatos</h2>
+
+        <h4 class="lbem">E-mail:<a href="mailto:contatodaempresa@empresa.com" target="_blank" class="email">contatodaempresa@empresa.com</a></h4>
+        <h4 class="lbtel">Telefone: 9999-9457<div class="telefone"></div></h4>
+        <h4 class="lbfac">Facebook: Web Security<div class="facebook"></div></h4>
+    </footer>
+</body>
+
+</html>
